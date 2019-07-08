74fe31e7162a411e847b6c8934ef77fe1c3b36e2
duydl-1764 <dang.le.duy@framgia.com>
2019-07-08 11:05:16 +0700

    add axios & saga

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import { Provider } from 'react-redux'
 import App from './App'
 import rootReducer from './reducers'
+import rootSaga from './sagas'
 
-const store = createStore(rootReducer)
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware),
+)
+
+sagaMiddleware.run(rootSaga)
 
 render(
   <Provider store={store}>
